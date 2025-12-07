d7ee52bac064c3e108e9bb10e532cbe50116b16e
Zuzk1 <brunoplata3@gmail.com>
2025-12-07 06:01:45 -0600

    okokok

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,17 +5,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // ¡ESTA LÍNEA ES CRUCIAL PARA LA LIBRERÍA DEL CALENDARIO!
+        // Repositorio para la librería del calendario
         maven { url = uri("https://jitpack.io") }
     }
 }
-
 rootProject.name = "ProyectoAula"
 include(":app")
     
